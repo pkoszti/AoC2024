9a6edfccb9ec41145dadd1c1fa61913f21b220c4
Pawel Kosztowniak <bodonoj@gmail.com>
2024-12-11 14:35:27 +0100

    AoC 2024 Day 11

---
+++ day11/part2.py
@@ -0,0 +1,28 @@
+with open('input2.txt') as f:
+    stones = dict.fromkeys(map(int, f.read().strip().split()), 1)
+
+
+def add_stone(stn):
+    if stn in temp.keys():
+        temp[stn] += amount
+    else:
+        temp[stn] = amount
+
+
+blinks = 75
+for i in range(0, blinks):
+    x = len(stones)
+    temp = {}
+    for stone, amount in stones.items():
+        if amount > 0:
+            if stone == 0:
+                add_stone(1)
+            elif len(str(stone)) % 2 == 0:
+                s = str(stone)
+                add_stone(int(s[:int(len(s) / 2)]))
+                add_stone(int(s[int(len(s) / 2):]))
+            else:
+                add_stone(stone * 2024)
+    stones = temp
+
+print(sum(stones.values()))
